2a872e1d90b39ff7d290fa70faa20df6966aadd4
igr76 <gritsuk766@gmail.com>
2024-06-10 20:49:11 +1000

    start

---
+++ src/main/java/com/example/serviceexpensesincome/ServiceExpensesIncomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceexpensesincome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExpensesIncomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceExpensesIncomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serviceexpensesincome/ServiceExpensesIncomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceexpensesincome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceExpensesIncomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
